815b1909ce751ab278ebe5b5b33cbe91ea9346c3
ksuthar937 <ksuthar937@gmail.com>
2024-04-12 13:13:40 +0530

    Implemented functionality requirements

--- script.js
+++ script.js
@@ -14,46 +14,66 @@ let form = document.querySelector("form");
 const taxModal = new bootstrap.Modal(document.getElementById("taxModal"));
 
 form.addEventListener("submit", (e) => {
-  if (!form.checkValidity()) {
-    e.preventDefault();
-    e.stopPropagation();
-    form.classList.add("was-validated");
-    return;
-  }
-
   e.preventDefault();
 
-  const { annualIncome, extraIncome, ageGroup, totalDeduct } = form.elements;
+  const numberFields = document.querySelectorAll('input[type="text"]');
+  let hasError = false;
 
-  const formData = {
-    annualIncome: Number(annualIncome.value),
-    extraIncome: Number(extraIncome.value),
-    ageGroup: ageGroup.value,
-    totalDeduction: Number(totalDeduct.value),
-  };
+  numberFields.forEach((field) => {
+    const value = field.value.trim();
+    const isValid = /^\d*\.?\d*$/.test(value);
 
-  console.log(formData);
+    const errorIcon = field.parentNode.querySelector(".input-group-text");
 
-  let overallIncome =
-    formData.annualIncome + formData.extraIncome - formData.totalDeduction;
+    if (!isValid) {
+      hasError = true;
+      errorIcon.style.display = "inline-block";
+    } else {
+      errorIcon.style.display = "hidden";
+    }
+  });
 
-  if (overallIncome > 800000) {
-    const taxableAmount = overallIncome - 800000;
-    let tax = 0;
+  if (!hasError) {
+    const { annualIncome, extraIncome, ageGroup, totalDeduct } = form.elements;
 
-    if (formData.ageGroup === "lesser") {
-      tax = 0.3 * taxableAmount;
-    } else if (formData.ageGroup === "between") {
-      tax = 0.4 * taxableAmount;
-    } else if (formData.ageGroup === "higher") {
-      tax = 0.1 * taxableAmount;
+    const formData = {
+      annualIncome: Number(annualIncome.value),
+      extraIncome: Number(extraIncome.value),
+      ageGroup: ageGroup.value,
+      totalDeduction: Number(totalDeduct.value),
+    };
+
+    console.log(!formData.annualIncome);
+
+    console.log(formData);
+
+    let overallIncome =
+      formData.annualIncome + formData.extraIncome - formData.totalDeduction;
+
+    if (overallIncome > 800000) {
+      const taxableAmount = overallIncome - 800000;
+      let tax = 0;
+
+      if (formData.ageGroup === "lesser") {
+        tax = 0.3 * taxableAmount;
+      } else if (formData.ageGroup === "between") {
+        tax = 0.4 * taxableAmount;
+      } else if (formData.ageGroup === "higher") {
+        tax = 0.1 * taxableAmount;
+      }
+
+      overallIncome = overallIncome - tax;
     }
 
-    overallIncome = overallIncome - tax;
+    document.getElementById("overallIncome").innerText =
+      overallIncome.toLocaleString();
+
+    taxModal.show();
   }
+});
 
-  document.getElementById("overallIncome").innerText =
-    overallIncome.toLocaleString();
+const close = document.getElementById("close");
 
-  taxModal.show();
+close.addEventListener("click", () => {
+  location.reload();
 });
